a2505072a2d0ddd8ee328b27ac5bec7f6b000cc5
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-09 22:26:36 -0600

    Added logic for starting REPL

--- monkey/main.go
+++ monkey/main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+
+	"github.com/VeryNotGood/monkey/repl"
+)
+
+func main() {
+
+	user, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Hello %s! Welcome to Monkey\n", user.Username)
+	repl.ReplStart(os.Stdin, os.Stdout)
+}
