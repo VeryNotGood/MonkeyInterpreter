d946e7b99fd407dd4b88a78000eee766cd818de1
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-09 22:10:46 -0600

    Initial commit

---
+++ monkey/repl/repl.go
@@ -0,0 +1,29 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/VeryNotGood/monkey/lexer"
+)
+
+func replStart(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, "M>>")
+
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != "EOF"; tok = l.NextToken() {
+			fmt.Fprintf(out, "%v\n", tok)
+		}
+	}
+}
