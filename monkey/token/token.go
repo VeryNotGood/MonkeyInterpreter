7d4aeeef8d5b5a956b63cf15fafaabb768cb99fa
sbelmore <shawnbelmore@MacBookPro.attlocal.net>
2024-12-09 21:50:35 -0600

    added EQ and NEQ identifiers

--- monkey/token/token.go
+++ monkey/token/token.go
@@ -19,6 +19,9 @@ const (
 	LT = "<"
 	GT = ">"
 
+	EQ  = "=="
+	NEQ = "!="
+
 	//Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
@@ -35,7 +38,7 @@ const (
 	FALSE    = "false"
 	IF       = "if"
 	ELSE     = "else"
-	RETURN     = "return"
+	RETURN   = "return"
 )
 
 type TokenType string
@@ -46,12 +49,12 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"fn":      FUNCTION,
-	"let":     LET,
-	"true":    TRUE,
-	"false":   FALSE,
-	"if":      IF,
-	"else":    ELSE,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
 	"return": RETURN,
 }
 
